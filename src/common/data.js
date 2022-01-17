879b00b774b9def883a33fca573822c4294899a4
Hardik Naik <hardiknaik7444@gmail.com>
2022-01-17 15:52:44 +0530

    data added

--- src/common/data.js
+++ src/common/data.js
@@ -1,13 +1,38 @@
 export const data = [
   {
-    month: "JAN",
+    month: "January",
     array: [
-      { date: "1", desc: "New Year" },
-      { date: "26", desc: "Republic Day" },
+      { date: "1/1/2022", desc: "New Year" },
+      { date: "1/26/2022", desc: "Republic Day" },
     ],
   },
   {
-    month: "FEB",
-    array: [{ date: "19", desc: "Chhatrapati Shivaji Maharaj Jayanti" }],
+    month: "February",
+    array: [{ date: "2/19/2022", desc: "Chhatrapati Shivaji Maharaj Jayanti" }],
+  },
+  {
+    month: "March",
+    array: [
+      { date: "3/1/2022", desc: "Maha Shivaratri" },
+      { date: "3/18/2022", desc: "Holi" },
+      { date: "3/19/2022", desc: "Holi" },
+    ],
+  },
+  {
+    month: "April",
+    array: [
+      { date: "4/2/2022", desc: "Gudi Padwa" },
+      { date: "4/10/2022", desc: "Ram Navami" },
+      { date: "4/14/2022", desc: "Ambedkar Jayanti" },
+      { date: "4/15/2022", desc: "Good Friday" },
+    ],
+  },
+  {
+    month: "May",
+    array: [
+      { date: "5/1/2022", desc: "Maharashtra Day" },
+      { date: "5/3/2022", desc: "Ramjan Eid" },
+      { date: "5/16/2022", desc: "Buddha Purnima" },
+    ],
   },
 ];
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import dayjs from "dayjs";
 import styles from "./Home.module.css";
 
 const Home = ({ data }) => {
@@ -8,14 +9,56 @@ const Home = ({ data }) => {
       <div className={styles.widget}>
         {data.array.map((date, index) => (
           <div key={index} className={styles.container}>
-              <div className={styles.mainline}></div>
-              <div className={classNames(styles.circleout,index%2===0?styles.evencircleout:styles.oddcircleout)}></div>
-              <div className={classNames(styles.circlein,index%2===0?styles.evencirclein:styles.oddcirclein)}></div>
-              <div className={classNames(styles.circlehalf,index%2===0?styles.evencirclehalf:styles.oddcirclehalf)}></div>
-              <div className={classNames(styles.line,index%2===0?styles.evenline:styles.oddline)}></div>
-              <div className={classNames(styles.smallcircle,index%2===0?styles.evensmallcircle:styles.oddsmallcircle)}></div>
-              <div className={classNames(styles.bigcircle,index%2===0?styles.evenbigcircle:styles.oddbigcircle)}>{date?.date}</div>
-              <div className={classNames(styles.textconainer,index%2===0?styles.eventextconainer:styles.oddtextconainer)}>{date?.desc}</div>
+            <div className={styles.mainline}></div>
+            <div
+              className={classNames(
+                styles.circleout,
+                index % 2 === 0 ? styles.evencircleout : styles.oddcircleout
+              )}
+            ></div>
+            <div
+              className={classNames(
+                styles.circlein,
+                index % 2 === 0 ? styles.evencirclein : styles.oddcirclein
+              )}
+            ></div>
+            <div
+              className={classNames(
+                styles.circlehalf,
+                index % 2 === 0 ? styles.evencirclehalf : styles.oddcirclehalf
+              )}
+            ></div>
+            <div
+              className={classNames(
+                styles.line,
+                index % 2 === 0 ? styles.evenline : styles.oddline
+              )}
+            ></div>
+            <div
+              className={classNames(
+                styles.smallcircle,
+                index % 2 === 0 ? styles.evensmallcircle : styles.oddsmallcircle
+              )}
+            ></div>
+            <div
+              className={classNames(
+                styles.bigcircle,
+                index % 2 === 0 ? styles.evenbigcircle : styles.oddbigcircle
+              )}
+            >
+              {dayjs(date?.date).format("DD")}
+            </div>
+            <div
+              className={classNames(
+                styles.textconainer,
+                index % 2 === 0
+                  ? styles.eventextconainer
+                  : styles.oddtextconainer
+              )}
+            >
+              <div>{date?.desc}</div>
+              <small>{dayjs(date?.date).format("ddd")}</small>
+            </div>
           </div>
         ))}
       </div>
